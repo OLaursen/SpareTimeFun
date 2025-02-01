3bf6b2313fbdc600850d79997ee11cf8de011197
olau <olau@itu.dk>
2025-02-01 18:29:02 +0100

    In progress with DifferentDistances

--- Kattis_CS/Kattis_CS/DifferentDistances.cs
+++ Kattis_CS/Kattis_CS/DifferentDistances.cs
@@ -1,6 +1,29 @@
 namespace Kattis_CS;
-
+//https://open.kattis.com/problems/differentdistances
+using System;
 public class DifferentDistances
 {
-    
+  public static void Main(string[] args)
+  {
+    while (true)
+    {
+      var numbers = new List<double>();
+      
+      var line = Console.ReadLine()!;
+      if (line.Equals("0")) 
+          break;
+      
+      foreach(var number in line.Replace(".", ",").Split(" "))
+      {
+        numbers.Add(double.Parse(number));
+      }
+      var p = numbers[4];
+      var x = Math.Pow(Math.Abs(numbers[0]-numbers[1]), p);
+      var y = Math.Pow(Math.Abs(numbers[2]-numbers[3]), p);
+
+      var pnorm = Math.Pow(x - y, 1.0d / p);
+      
+      Console.WriteLine();
+    }
+  }
 }
